19b10e61210dbbe841cbe1a84b53543f238bd821
Arnav <arnavmodi23@gmail.com>
2024-12-31 11:32:12 +0530

    Changes to be committed:

--- Sus/Sus/settings.py
+++ Sus/Sus/settings.py
@@ -124,5 +124,9 @@ USE_TZ = True
 
 # settings.py
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '/Sus/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
