377d8d6c4516cd40efd9db4ea1b09e10c1f74ab7
Arnav <arnavmodi23@gmail.com>
2025-01-01 17:46:12 +0530

    Changes to be committed:

--- Sus/Sus/settings.py
+++ Sus/Sus/settings.py
@@ -80,8 +80,9 @@ WSGI_APPLICATION = 'Sus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'phishing_database',
+        'ENHOST': os.environ.get('MONGODB_URL'),  # Using the MongoDB URL from the environment variable
     }
 }
 
